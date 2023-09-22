8f7fa5007e353d0bdb1abd362c35e5e7d6aa8253
Juan Villegas <juanvj0425@gmail.com>
2023-09-22 17:30:54 -0500

    feat: sidebar, feed, follow, modals UI

---
+++ components/Modal.tsx
@@ -0,0 +1,69 @@
+import { useCallback } from 'react';
+
+interface ModalProps {
+    isOpen?: boolean;
+    onClose: () => void;
+    onSubmit: () => void;
+    title?: string;
+    body?: React.ReactElement;
+    footer?: React.ReactElement;
+    actionLabel: string;
+    disabled?: boolean;
+}
+
+const Modal: React.FC<ModalProps> = ({
+    isOpen,
+    onClose,
+    onSubmit,
+    title,
+    body,
+    footer,
+    actionLabel,
+    disabled
+}) => {
+
+    const handleClose = useCallback(() => {
+        if (disabled) {
+            return;
+        }
+
+        onClose();
+    }, [disabled, onClose]);
+
+    const handleSubmit = useCallback(() => {
+        if (disabled) {
+            return;
+        }
+
+        onSubmit();
+    }, [disabled, onSubmit]);
+
+    if (!isOpen){
+        return null;
+    }
+
+    return(
+        <>
+            <div 
+                className='
+                    justify-center
+                    items-center
+                    flex
+                    overflow-x-hidden
+                    overflow-y-auto
+                    fixed
+                    inset-0
+                    z-50
+                    outline-none
+                    focus:outline-none
+                    bg-neutral-800
+                    bg-opacity-70
+                '
+            >
+
+            </div>
+        </>
+    )
+}
+
+export default Modal;
