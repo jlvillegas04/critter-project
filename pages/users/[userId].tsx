8fb78601e4cf6402c3990839b447caa300e2e00d
Juan Villegas <juanvj0425@gmail.com>
2023-09-22 22:10:45 -0500

    feat: user profile basic layout

---
+++ pages/users/[userId].tsx
@@ -0,0 +1,36 @@
+import Header from "@/components/Header";
+import useUser from "@/hooks/useUser";
+import UserHero from "@/components/users/UserHero";
+import UserBio from "@/components/users/UserBio";
+import { useRouter } from "next/router";
+import { ClipLoader } from "react-spinners";
+
+const UserView = () => {
+    const router = useRouter();
+    const { userId } = router.query;
+
+    const { data: fetchedUsed, isLoading } = useUser(userId as string);
+    
+    if (isLoading || !fetchedUsed) {
+        return (
+            <div className="
+                flex
+                justify-center
+                items-center
+                h-full
+            ">
+                <ClipLoader color="lightblue" size={80} />
+            </div>
+        )
+    }
+
+    return (
+        <>
+            <Header showBackArrow label={fetchedUsed?.name} />
+            <UserHero userId={userId as string} />
+            <UserBio userId={userId as string} />
+        </>
+    );
+}
+
+export default UserView;
