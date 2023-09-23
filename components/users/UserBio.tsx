680633821b3bb4516f7f4705071f8c228b71327d
Juan Villegas <juanvj0425@gmail.com>
2023-09-23 11:43:20 -0500

    feat: profile edit logic and bio

--- components/users/UserBio.tsx
+++ components/users/UserBio.tsx
@@ -4,6 +4,8 @@ import useCurrentUser from "@/hooks/useCurrentUser";
 import useUser from "@/hooks/useUser";
 import { useMemo } from 'react';
 import Button from '../Button';
+import { BiCalendar } from 'react-icons/bi';
+import useEditModal from '@/hooks/useEditModal';
 
 interface UserBioProps {
     userId: string;
@@ -12,6 +14,8 @@ interface UserBioProps {
 const UserBio: React.FC<UserBioProps> = ({ userId }) => {
     const { data: currentUser } = useCurrentUser();
     const { data: fetchedUser } = useUser(userId);
+
+    const editModal = useEditModal();
 
     const createdAt = useMemo(() => {
         if (!fetchedUser?.createdAt) {
@@ -24,7 +28,7 @@ const UserBio: React.FC<UserBioProps> = ({ userId }) => {
         <div className='border-b-[1px] border-neutral-800 pb-4'>
             <div className='flex justify-end p-2'>
                 {currentUser?.id === userId ? (
-                    <Button secondary label="Edit" onClick={() => {}} />
+                    <Button secondary label="Edit" onClick={editModal.onOpen} />
                 ) : (
                     <Button 
                         onClick={() => {}}
@@ -32,6 +36,54 @@ const UserBio: React.FC<UserBioProps> = ({ userId }) => {
                         secondary
                     />
                 )}
+            </div>
+            <div className='mt-8 px-4'>
+                    <div className='flex flex-col'>
+                        <p className='text-white text-2xl font-semibold'>
+                            {fetchedUser?.name}
+                        </p>
+                        <p className='text-md text-neutral-500'>
+                            @{fetchedUser?.username}
+                        </p>
+                    </div>
+                    <div className='flex flex-col mt-4'>
+                        <p className='text-white'>
+                            {fetchedUser?.bio}
+                        </p>
+                        <div 
+                            className='
+                                flex
+                                flex-row
+                                items-center
+                                gap-2
+                                mt-4
+                                text-neutral-500
+                            '
+                        >
+                            <BiCalendar size={24}/>
+                            <p>
+                                Joined {createdAt}
+                            </p>
+                        </div>                 
+                    </div>
+                    <div className='flex flex-row items-center mt-4 gap-6'>
+                        <div className='flex flex-row items-center gap-1'>
+                            <p className='text-white'>
+                                {fetchedUser?.followingIds?.length}
+                            </p>
+                            <p className='text-neutral-500'>
+                                Following
+                            </p>
+                        </div>
+                        <div className='flex flex-row items-center gap-1'>
+                            <p className='text-white'>
+                                {fetchedUser?.followersCount || 0}
+                            </p>
+                            <p className='text-neutral-500'>
+                                Followers
+                            </p>
+                        </div>
+                    </div>
             </div>
         </div>
     );
