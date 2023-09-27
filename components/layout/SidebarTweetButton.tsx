9fe39336383b70caf30d0aa47b1f8956d46c0f8c
Juan Villegas <juanvj0425@gmail.com>
2023-09-27 10:53:19 -0500

    fix: login modal popup when logged user clicks tweet button

--- components/layout/SidebarTweetButton.tsx
+++ components/layout/SidebarTweetButton.tsx
@@ -2,14 +2,20 @@ import { useRouter } from "next/router";
 import { FaFeather } from 'react-icons/fa'
 import { useCallback } from 'react';
 import useLoginModal from "@/hooks/useLoginModal";
+import useCurrentUser from "@/hooks/useCurrentUser";
 
 const SidebarTweetButton = () => {
     const router = useRouter();
     const loginModal = useLoginModal();
+    const { data: currentUser } = useCurrentUser();
 
     const onClick = useCallback(() => {
-        loginModal.onOpen();
-    }, [loginModal]);
+        if (!currentUser) {
+            return loginModal.onOpen();
+        }
+
+        router.push('/');
+    }, [loginModal, router, currentUser]);
     return (
         <div onClick={onClick}>
             <div
